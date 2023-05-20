be2e681c8d8fca1f3da58385ada9cd9ecd0a1fe1
Sachith <110117965+sacheex@users.noreply.github.com>
2023-05-20 10:29:31 +0530

    Update

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,5 @@ const fileLabel = document.getElementById('audio-label');
 fileInput.addEventListener('change', function() {
   fileLabel.textContent = fileInput.files[0].name;
 });
-
 const para = document.getElementById("text");
 para.classList.add("appear")
