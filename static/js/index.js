f76646b664dc3fa47db20875fcc1049ee2de0aff
sacheex <sachithdhanushka00@gmail.com>
2023-05-20 10:25:50 +0530

    add new features

--- static/js/index.js
+++ static/js/index.js
@@ -4,3 +4,6 @@ const fileLabel = document.getElementById('audio-label');
 fileInput.addEventListener('change', function() {
   fileLabel.textContent = fileInput.files[0].name;
 });
+
+const para = document.getElementById("text");
+para.classList.add("appear")
