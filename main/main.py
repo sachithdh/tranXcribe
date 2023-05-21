9bc8f8ec74fa14229c1faf7353f78342c47e595f
Sachith <110117965+sacheex@users.noreply.github.com>
2023-05-21 17:43:17 +0530

    Add comments

--- main/main.py
+++ main/main.py
@@ -2,8 +2,9 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # load environment variables from a .env file
 
+# Transcribe audio file
 def transcribe(audio_file):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
