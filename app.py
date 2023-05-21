974b0bdcd9bcdc0501df37650e7c3182104f5c44
Sachith <110117965+sacheex@users.noreply.github.com>
2023-05-21 17:38:51 +0530

    Add comments

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 from main.main import transcribe
 
+# Initialize the app
 app = Flask(__name__)
 app.secret_key = "key"
 
+# Landing page
 @app.route("/")
 def home():
     return render_template("base.html")
 
+# Get the audio file and pass it into transcribe function 
 @app.route("/generate", methods=["POST", "GET"])
 def generate():
     if request.method == "POST":
@@ -17,7 +20,7 @@ def generate():
         audio = open("new.mp3", "rb")
         transcript = transcribe(audio)
         
-        return render_template("index.html", text = transcript)
+        return render_template("index.html", text = transcript) 
     
     return redirect(url_for("home"))
 
